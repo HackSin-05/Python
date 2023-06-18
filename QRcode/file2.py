e2e1282a0a4621b5f91ceeb67f5f5e0ab9d76a89
Sujay Deshpande <105727756+Sujay-Deshpande@users.noreply.github.com>
2023-06-18 19:32:20 +0530

    Hacksome: Adding comments

--- QRcode/file2.py
+++ QRcode/file2.py
@@ -5,11 +5,11 @@ from PIL import Image
 qr = qrcode.QRCode(version=1,
                    error_correction=qrcode.constants.ERROR_CORRECT_H,
                    box_size=10,
-                   border=3)
+                   border=3) # Replace box-size & border with desire box size & border thickness
 
-qr.add_data("https://kunal-khairnar-05.github.io/Portfolio/")
+qr.add_data("https://kunal-khairnar-05.github.io/Portfolio/") # Enter your desire data that should be put into qrcode
 qr.make(fit=True)
 
-img = qr.make_image(fill_color="red",back_color="white")
+img = qr.make_image(fill_color="red",back_color="white") # fill_color for the qrcode color & back_color for background color
 
-img.save("portfolio2.png")
+img.save("portfolio2.png") # replace the "portfolio2.png" with your desire file name
