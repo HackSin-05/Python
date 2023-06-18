4496d5055cfba4e13f42b67df698bf33d2695512
Sujay Deshpande <105727756+Sujay-Deshpande@users.noreply.github.com>
2023-06-18 19:21:54 +0530

    Hacksome: Adding comments main.py

--- videoToGIF/main.py
+++ videoToGIF/main.py
@@ -1,5 +1,5 @@
 from moviepy.editor import *
 
-video = VideoFileClip(PATH_TO_FILE).subclip(00,5)
+video = VideoFileClip(PATH_TO_FILE).subclip(00,5) # Replace PATH_TO_FILE with your video file.
 
-video.write_gif("test1.gif")
+video.write_gif("test1.gif") # Replace "test1.gif" with your desire gif file name.
