eda2022e1596017de126a767a22892d5ab3511f8
Sujay Deshpande <105727756+Sujay-Deshpande@users.noreply.github.com>
2023-06-18 19:35:56 +0530

    Hacksome: Adding comments

--- QRcode/qrcodegeneration.py
+++ QRcode/qrcodegeneration.py
@@ -1,5 +1,5 @@
 import qrcode as qr
 
-img = qr.make("https://kunal-khairnar-05.github.io/Portfolio/")
+img = qr.make("https://kunal-khairnar-05.github.io/Portfolio/") # Replace "https://kunal-khairnar-05.github.io/Portfolio/" with your desire location that you want to put into qrcode
 
-img.save("my_portfolio.png")
+img.save("my_portfolio.png") # Replace "my_portfolio.png" With your desire file name
